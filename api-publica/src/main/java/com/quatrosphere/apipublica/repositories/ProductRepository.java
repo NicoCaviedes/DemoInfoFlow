3f9a87bf995a93f3cf2c863ce69c0aaabe80a261
NicoCaviedes <ni.caviedes@duocuc.cl>
2023-10-28 16:43:05 -0300

    Reset Branch

--- api-publica/src/main/java/com/quatrosphere/apipublica/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quatrosphere.apipublica.repositories;
-
-import com.quatrosphere.apipublica.models.ProductModel;
-
-public interface ProductRepository extends BaseRepository<ProductModel> {
-    
-}
